72054d0cf6ac713def7d5443e12c1725564d0b47
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-03-02 14:19:56 -0800

    create sign up page bones

--- src/Auth/SignUpPage.js
+++ src/Auth/SignUpPage.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 export default class SignUpPage extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value})
+    }
+    handlePasswordChange = (e) => {
+        this.setState({password: e.target.value})
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        //api stuff goes here
+    }
     render() {
+        console.log(this.state);
         return (
             <div>
-                signup
+                <form>
+                    <label> Email
+                        <input value = {this.state.email} onChange = {this.handleEmailChange}/>
+                    </label>
+                    <label> Password
+                        <input value = {this.state.password} onChange = {this.handlePasswordChange}/>
+                    </label>
+                    <button>Sign Up</button>
+                </form>
             </div>
         )
     }
